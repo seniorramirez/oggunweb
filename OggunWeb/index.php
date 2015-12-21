5e2d73d422f8bdc080dd5d83c0da72f19a5d5d01
senior ramirez <kev.ramirez1996@gmail.com>
2015-12-20 21:08:54 -0500

    Revert "oggunweb"
    This reverts commit 6d7dc95946f6cafcaf753d18503facc7a8cb8772.

--- OggunWeb/index.php
+++
@@ -1,7 +0,0 @@
-﻿
-<?php
-//session_start ();
-
-header('location:php/portada.php',"Content-Type: text/html;charset=utf-8");
-
-?>
--- OggunWeb/php/salir.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start ();
-
-mysqli_close($conexion)or die('No se pudo desconectar de la Base de Datos');
-
-
-?>
